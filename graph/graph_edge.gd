a2b05802746fdf5aba07b01b9697a5176c97a6ef
corneey <konstantinbock@yahoo.de>
2024-08-17 17:30:00 +0200

    Refactor graph_edge.gd

--- graph/graph_edge.gd
+++ graph/graph_edge.gd
@@ -3,13 +3,11 @@ class_name GraphEdge extends Resource
 var neighbor
 var weight: int
 
-
 #Constructor, Time O(1) Space O(1)
-@warning_ignore("shadowed_variable")
-func _init(neighbor, weight):
-	self.neighbor = neighbor
-	self.weight = weight
+func _init(_neighbor, _weight):
+	self.neighbor = _neighbor
+	self.weight = _weight
  
 #Time O(1) Space O(1)
 func _to_string() -> String:
-	return "(" + str(self.neighbor) + "," + str(self.weight) + ")"
+	return "(" + str(neighbor) + "," + str(weight) + ")"
---
+++ tile.gd
@@ -0,0 +1,102 @@
+extends Node2D
+class_name Tile
+
+var grid: Grid
+
+@export 
+var x_location : int
+@export 
+var y_location : int
+@export 
+var is_filled : bool = false
+@export 
+var is_placing_allowed : bool
+@export
+var placedTile : Tile_Type
+
+func _ready() -> void:
+	print()
+
+func _fill_tile(tile_enum : String, rotation_degree : int):
+	placedTile = Tile_Type.new(tile_enum, rotation_degree)
+	$Sprite2D.modulate = Color.GREEN
+	print("Test")
+	if(_is_tile_placeable(placedTile)):
+		is_filled = true
+
+func _is_tile_placeable(placedTile : Tile_Type) -> bool:
+	var neighbours : Array = grid._return_neighbours(placedTile)
+
+	#Connection nach Norden
+	if(neighbours[0] != null):
+		var this_connection = _get_connection_type(placedTile, Direction_Enum.North);
+		var neighbour_connection = _get_connection_type(neighbours[0], Direction_Enum.South);
+		if(this_connection != Transport_Enum.None and this_connection == neighbour_connection):
+			return true
+	if(neighbours[1] != null):
+		var this_connection = _get_connection_type(placedTile, Direction_Enum.East);
+		var neighbour_connection = _get_connection_type(neighbours[0], Direction_Enum.West);
+		if(this_connection != Transport_Enum.None and this_connection == neighbour_connection):
+			return true
+	if(neighbours[2] != null):
+		var this_connection = _get_connection_type(placedTile, Direction_Enum.South);
+		var neighbour_connection = _get_connection_type(neighbours[0], Direction_Enum.North);
+		if(this_connection != Transport_Enum.None and this_connection == neighbour_connection):
+			return true
+	if(neighbours[3] != null):
+		var this_connection = _get_connection_type(placedTile, Direction_Enum.West);
+		var neighbour_connection = _get_connection_type(neighbours[0], Direction_Enum.East);
+		if(this_connection != Transport_Enum.None and this_connection == neighbour_connection):
+			return true
+	return false
+			
+func _get_connection_type(input_tile : Tile_Type, direction):
+		match direction:
+			Direction_Enum.North:
+				if(input_tile.car_n_e 
+				or input_tile.car_n_s
+				or input_tile.car_n_w):
+					return Transport_Enum.Car
+				elif(input_tile.train_n_e 
+				or input_tile.train_n_s
+				or input_tile.train_n_w):
+					return Transport_Enum.Train
+				else:
+					return Transport_Enum.None
+			Direction_Enum.West:
+				if(input_tile.car_w_e 
+				or input_tile.car_w_s
+				or input_tile.car_n_w):
+					return Transport_Enum.Car
+				elif(input_tile.train_w_e 
+				or input_tile.train_w_s
+				or input_tile.train_n_w):
+					return Transport_Enum.Train
+				else:
+					return Transport_Enum.None
+			Direction_Enum.South:
+				if(input_tile.car_s_e 
+				or input_tile.car_w_s
+				or input_tile.car_n_s):
+					return Transport_Enum.Car
+				elif(input_tile.train_s_e 
+				or input_tile.train_w_s
+				or input_tile.train_n_s):
+					return Transport_Enum.Train
+				else:
+					return Transport_Enum.None
+			Direction_Enum.East:
+				if(input_tile.car_s_e 
+				or input_tile.car_w_e
+				or input_tile.car_n_e):
+					return Transport_Enum.Car
+				elif(input_tile.train_s_e 
+				or input_tile.train_w_e
+				or input_tile.train_n_e):
+					return Transport_Enum.Train
+				else:
+					return Transport_Enum.None
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	_fill_tile("Test", 0)
